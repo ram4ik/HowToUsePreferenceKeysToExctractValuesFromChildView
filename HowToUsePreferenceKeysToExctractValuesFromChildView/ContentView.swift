b18140841954652ad0a0a7f14d91f2ab403cade5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-28 19:13:41 +0300

    Initial Commit

---
+++ HowToUsePreferenceKeysToExctractValuesFromChildView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToUsePreferenceKeysToExctractValuesFromChildView
+//
+//  Created by ramil on 28.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToUsePreferenceKeysToExctractValuesFromChildView/HowToUsePreferenceKeysToExctractValuesFromChildViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUsePreferenceKeysToExctractValuesFromChildViewApp.swift
+//  HowToUsePreferenceKeysToExctractValuesFromChildView
+//
+//  Created by ramil on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUsePreferenceKeysToExctractValuesFromChildViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
