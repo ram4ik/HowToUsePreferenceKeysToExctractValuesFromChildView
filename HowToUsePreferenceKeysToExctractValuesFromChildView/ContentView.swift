38af5c752906264f15424de59ff3cfa4e860e637
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-28 19:31:15 +0300

    init

--- HowToUsePreferenceKeysToExctractValuesFromChildView/ContentView.swift
+++ HowToUsePreferenceKeysToExctractValuesFromChildView/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text: String = "Hello, world!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                SecondaryScreen(text: text)
+                    .navigationTitle("Navigation Title")
+                    .preference(key: CustomTitlePreferenceKey.self, value: "NEW VALUE")
+            }
+        }
+        .onPreferenceChange(CustomTitlePreferenceKey.self) { value in
+            self.text = value
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SecondaryScreen: View {
+    
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+}
+
+struct CustomTitlePreferenceKey: PreferenceKey {
+    
+    static var defaultValue: String = ""
+    
+    static func reduce(value: inout String, nextValue: () -> String) {
+        value = nextValue()
     }
 }
